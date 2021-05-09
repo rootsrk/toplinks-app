0c338fd5014ede526b08301eed7f69de02728436
sumedharkulkarni <sumedha.rk.05@gmail.com>
2021-05-09 17:11:14 +0530

    Altered css structurally

--- client/src/components/card/card.js
+++ client/src/components/card/card.js
@@ -2,18 +2,18 @@ function Card(props) {
   const { userData } = props;
   const { profile_image_url, count, name, screen_name } = userData;
   return (
-    <div className='home-content-stats-content'>
-      <div className='home-content-stats-icon-container'>
+    <div className='custom-card'>
+      <div className='custom-card-icon-container'>
         <img
-          className='home-content-stats-icon rounded-edges'
+          className='custom-card-icon rounded-edges'
           src={profile_image_url}
           alt='leader board icon'
         />
-        <span className='home-content-stats-count'>
+        <span className='custom-card-count'>
           {count < 10 ? `0${count}` : count}
         </span>
       </div>
-      <div className='home-content-stats-content-label'>
+      <div className='custom-card-content-label'>
         <p>{name}</p>
         <span>{`@${screen_name}`}</span>
       </div>
